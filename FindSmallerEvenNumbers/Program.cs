1a7cf07a344a845acb604871c562b91e3cf2833a
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-12 23:16:26 +0700

    FindSmallerEvenNumbers

---
+++ FindSmallerEvenNumbers/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+int NextEvenNumber = 2;
+    while (NextEvenNumber <= number)
+        {
+            Console.WriteLine (NextEvenNumber);
+            NextEvenNumber = NextEvenNumber + 2;
+        }
